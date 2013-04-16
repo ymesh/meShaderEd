ecf3a02aa086f9e460dccf4f9e2c5706137280d7
ymesh <Yuri.Meshalkin@gmail.com>
2013-04-16 19:27:16 +0300

    Change version to 0.3.0

--- meShaderEd.py
+++ meShaderEd.py
@@ -2,15 +2,7 @@
 #
 # meShaderEd.py
 #
-# version 0.2.9 (19 Mar 2013)
-# version 0.2.8 (20 Jan 2012)
-# version 0.2.7 (20 Sep 2012)
-# version 0.2.6 (29 Aug 2012)
-# version 0.2.5 (19 Jul 2012)
-# version 0.2.0 (27 May 2012)
-# version 0.1.1 (16 May 2012)
-# version 0.1.0 (13 May 2012)
-# version 0.0.1 (5 Mar 2011)
+# version 0.3.0 (16 Apr 2013)
 #
 # written by Yuri.Meshalkin (mesh@kpp.kiev.ua)
 #
@@ -40,7 +32,7 @@ from global_vars import app_global_vars, DEBUG_MODE
 
 
 root = normPath ( sys.path[0] )
-version = '0.2.9'
+version = '0.3.0'
 
 app_settings = QtCore.QSettings ( QtCore.QSettings.IniFormat,
                                   QtCore.QSettings.UserScope,
